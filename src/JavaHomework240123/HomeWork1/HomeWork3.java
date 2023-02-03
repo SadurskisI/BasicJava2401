ad2579daf97b77e7aa6d66922800dd5d7b4b91ef
SadurskisI <volk741852@gmail.com>
2023-02-03 14:54:51 +0200

    Lesson3

--- src/JavaHomework240123/HomeWork1/HomeWork3.java
+++ src/JavaHomework240123/HomeWork1/HomeWork3.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class HomeWork3 {
     public static void main(String[] args) {
+/* Это при условии ввода слов пользователем//Вариант 1
 
         Scanner scanner = new Scanner(System.in);
         System.out.print("Введите первое слово: ");
@@ -13,6 +14,17 @@ public class HomeWork3 {
         int half = word1.length() / 2;
         String word3 = word1.substring(0, half) + word2.substring(half);
         System.out.println("Новое слово: " + word3);
+  */
+        //Вариант 2
+        String word1 = "ГОГА";
+        String word2 = "РОГА";
+        int half = word1.length() / 2;
+        String word3 = word1.substring(0, half) + word2.substring(half);
+        System.out.println("Первое слово - "+ word1);
+        System.out.println("Второе слово - "+ word2);
+        System.out.println("Новое слово - " + word3);
+
+
     }
 }
 
