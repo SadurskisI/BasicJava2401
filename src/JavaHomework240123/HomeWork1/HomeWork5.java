e9ef4668d801bb94ce96eaf7b409baa183bd6ec5
SadurskisI <volk741852@gmail.com>
2023-02-03 11:41:10 +0200

    Lesson3

--- src/JavaHomework240123/HomeWork1/HomeWork5.java
+++ src/JavaHomework240123/HomeWork1/HomeWork5.java
@@ -1,4 +1,26 @@
 package JavaHomework240123.HomeWork1;
 
+import java.util.Scanner;
+
 public class HomeWork5 {
+    //////////////Первый вариант - курс доллара берёт из кода
+    /*  public static void main(String[] args) {
+        System.out.println("Пожалуйста введите количество EURO");
+        Scanner scanner = new Scanner(System.in);
+        double euro = scanner.nextInt();
+        double dollar = euro * 1.09;
+        System.out.println("Это будет - " + dollar + " долларов" );
+*/
+    ///////////////// Второй вариант - когда актуальный курс вводит пользователь
+        public static void main(String[] args) {
+            System.out.println("Пожалуйста введите количество EURO");
+            Scanner scanner = new Scanner(System.in);
+            double euro = scanner.nextDouble();
+            System.out.println("Теперь ввидите актуальный курс доллара");
+            double dollarValue = scanner.nextDouble();
+            double dollar = euro * dollarValue;
+            System.out.println("Это будет - " + dollar + " долларов" );
+
+
+    }
 }
